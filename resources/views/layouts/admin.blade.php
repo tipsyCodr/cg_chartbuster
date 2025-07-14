7a6eceb5724bd6f9309e226faf01d51fbe0730f0
Hemant Kumar <itshemant566@gmail.com>
2025-07-14 13:50:44 +0530

    asdf

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
+
     <title>Admin Dashboard | {{ config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
